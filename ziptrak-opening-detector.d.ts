7ac248c1a6de198966d4fdeac9414c8ece1757fd
Matthew Freak <fremk005@mymail.unisa.edu.au>
2024-10-25 17:01:58 +1030

    Added type declarations for TypeScript compatibility

---
+++ ziptrak-opening-detector.d.ts
@@ -0,0 +1,19 @@
+declare module 'ziptrak-opening-detector' {
+
+    /**
+     * Detect openings of an image
+     *
+     * @param {HTMLImageElement} image - The image element to detect openings from
+     * @param {Array<HTMLCanvasElement>} canvasSlotList - A list of canvas elements to show each detection step, can be an empty array
+     * @return {Array<Array<number>>} A 2D array where each inner array represents the four corner coordinates of a quad
+     */
+    export function autoDetectBlindOpenings(image: HTMLImageElement, canvasSlotList?: Array<HTMLCanvasElement>): Array<Array<number>>;
+  
+    /**
+     * Detect openings of an image based on user input
+     *
+     * @param {Array<number>} userCoordinates - An array of form [x1, y1, x2, y2, x3, y3, x4, y4] representing the coordinates of the quad
+     * @return {Array<number>} An array of form [x1, y1, x2, y2, x3, y3, x4, y4] representing the corner coordinates of the detected quad
+     */
+    export function manualDetectBlindOpenings(userCoordinates: Array<number>): Array<number>;
+}
